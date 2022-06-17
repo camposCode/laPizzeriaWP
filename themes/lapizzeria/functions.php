8c7ecea50610605c3b2e18d9d3326f305e9ebedc
camposCode <info@josecampos.io>
2022-06-17 16:04:53 +0200

    menu style done

--- themes/lapizzeria/functions.php
+++ themes/lapizzeria/functions.php
@@ -5,7 +5,12 @@ function lapizzeria_styles(){
     wp_enqueue_style('googlefonts', 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Raleway:wght@100;400;700&display=swap', array(), '1.0.0');
     wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0.0');
 
-    wp_enqueue_script('slicknavjs', 'https://cdnjs.cloudflare.com/ajax/libs/SlickNav/1.0.10/slicknav.min.css', array('jquery'), '1.0.10', true);
+    wp_enqueue_style('slicknavcss', 'https://cdnjs.cloudflare.com/ajax/libs/SlickNav/1.0.10/slicknav.min.css', array(), '1.0.10');
+
+     // Scripts
+     wp_enqueue_script('slicknavjs', 'https://cdnjs.cloudflare.com/ajax/libs/SlickNav/1.0.10/jquery.slicknav.min.js', array('jquery'), '1.0.10', true);
+
+     wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'lapizzeria_styles');
---
+++ themes/lapizzeria/js/scripts.js
@@ -0,0 +1,4 @@
+jQuery(document).ready($ => {
+    // Menu responsive
+    $('#menu ul').slicknav();
+});
