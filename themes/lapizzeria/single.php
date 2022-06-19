81d79f3e43dd7d9b7ad3d6ac7dc6b9e7e138b0b0
camposCode <info@josecampos.io>
2022-06-19 10:00:39 +0200

    comments form added and comments displayed

--- themes/lapizzeria/single.php
+++ themes/lapizzeria/single.php
@@ -2,6 +2,9 @@
 
 <?php while(have_posts()) : the_post(); 
         get_template_part('template-parts/loop', 'contenido');
+
+        // Commentarios
+        comments_template();
 endwhile; ?>
 
 <?php get_footer(); ?>
